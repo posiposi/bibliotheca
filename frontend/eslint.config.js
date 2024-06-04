05bb099f48b0135d2c4fbb1ae6d966461391d3de
daichi.sugiyama <caad12.di2@gmail.com>
2024-06-05 00:07:05 +0900

    flat config移行

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,25 +1,49 @@
-import { Linter } from "eslint";
+import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier";
+import html from "eslint-plugin-html";
+import markdown from "eslint-plugin-markdown";
+import js from "@eslint/js";
+import typeScriptESLint from "@typescript-eslint/eslint-plugin";
+import typeScriptESLintParser from "@typescript-eslint/parser";
 
-/** @type {Linter.Config} */
-const config = {
-  files: ["**/*.js", "**/*.ts", "**/*.jsx", "**/*.tsx"],
-  languageOptions: {
-    ecmaVersion: 12,
-    sourceType: "module",
-    globals: {
-      Atomics: "readonly",
-      SharedArrayBuffer: "readonly",
+const compat = new FlatCompat();
+
+export default [
+  {
+    ignores: ["**/fixtures/**", "**/dist/**"],
+  },
+  js.configs.recommended,
+  eslintConfigPrettier,
+  ...compat.extends(
+    "plugin:node/recommended",
+    "plugin:@typescript-eslint/eslint-recommended"
+  ),
+  {
+    plugins: {
+      typeScriptESLint,
+      html,
+      markdown,
+    },
+    languageOptions: {
+      globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+      },
+      parser: typeScriptESLintParser,
+      parserOptions: {
+        sourceType: "module",
+        ecmaVersion: 2020,
+      },
+    },
+    rules: {
+      "no-console": "off",
+      "no-debugger": "error",
+      "node/no-deprecated-api": "off",
+      "node/no-unpublished-import": "off",
+      "node/no-unpublished-require": "off",
+      "node/no-unsupported-features/es-syntax": "off",
+      "no-process-exit": "off",
+      "node/no-missing-import": "off",
     },
   },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
-  rules: {
-    "react/react-in-jsx-scope": "off",
-  },
-  extends: ["eslint:recommended"],
-};
-
-export default config;
+];
