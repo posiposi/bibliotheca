6cc39f0f9ed97266c4fea6e94b37112357faf102
daichi.sugiyama <caad12.di2@gmail.com>
2024-06-04 23:37:12 +0900

    flat config形式に修正

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,4 +1,7 @@
 import { Linter } from "eslint";
+import eslintRecommended from "eslint/conf/eslint-recommended";
+import reactRecommended from "eslint-plugin-react";
+import typescriptRecommended from "@typescript-eslint/eslint-plugin/dist/configs/recommended";
 
 /** @type {Linter.Config} */
 const config = {
@@ -11,11 +14,6 @@ const config = {
       SharedArrayBuffer: "readonly",
     },
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:typescript/recommended",
-  ],
   plugins: ["react"],
   settings: {
     react: {
@@ -25,6 +23,7 @@ const config = {
   rules: {
     "react/react-in-jsx-scope": "off",
   },
+  override: [eslintRecommended, reactRecommended, typescriptRecommended],
 };
 
 export default config;
