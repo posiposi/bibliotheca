4c6dd1fb45b4a22b0ddd169e3c2cc6663fb1dc69
daichi.sugiyama <caad12.di2@gmail.com>
2024-06-04 23:30:25 +0900

    設定ファイルをフォーマット

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,27 +4,22 @@ import { Linter } from "eslint";
 const config = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-  ],
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:typescript/recommended"],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module"
+    sourceType: "module",
   },
-  plugins: [
-    "react"
-  ],
+  plugins: ["react"],
   settings: {
     react: {
-      version: "detect"
-    }
+      version: "detect",
+    },
   },
   rules: {
-    "react/react-in-jsx-scope": "off"
-  }
+    "react/react-in-jsx-scope": "off",
+  },
 };
 
 export default config;
