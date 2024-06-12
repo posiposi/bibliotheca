222190ccbbfb70dd4c350b858649ecdffc9aacff
daichi.sugiyama <caad12.di2@gmail.com>
2024-06-12 23:07:17 +0900

    #2 ESLint設定追加

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,49 +1,23 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import eslintConfigPrettier from "eslint-config-prettier";
-import html from "eslint-plugin-html";
-import markdown from "eslint-plugin-markdown";
-import js from "@eslint/js";
-import typeScriptESLint from "@typescript-eslint/eslint-plugin";
-import typeScriptESLintParser from "@typescript-eslint/parser";
-
-const compat = new FlatCompat();
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigRules } from "@eslint/compat";
 
 export default [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
   {
-    ignores: ["**/fixtures/**", "**/dist/**"],
-  },
-  js.configs.recommended,
-  eslintConfigPrettier,
-  ...compat.extends(
-    "plugin:node/recommended",
-    "plugin:@typescript-eslint/eslint-recommended"
-  ),
-  {
-    plugins: {
-      typeScriptESLint,
-      html,
-      markdown,
-    },
-    languageOptions: {
-      globals: {
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly",
-      },
-      parser: typeScriptESLintParser,
-      parserOptions: {
-        sourceType: "module",
-        ecmaVersion: 2020,
-      },
-    },
+    languageOptions: { globals: globals.browser },
     rules: {
-      "no-console": "off",
-      "no-debugger": "error",
-      "node/no-deprecated-api": "off",
-      "node/no-unpublished-import": "off",
-      "node/no-unpublished-require": "off",
-      "node/no-unsupported-features/es-syntax": "off",
-      "no-process-exit": "off",
-      "node/no-missing-import": "off",
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   },
 ];
