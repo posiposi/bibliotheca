c1a948bcfacb2c524a243da56225324b8b045b8c
daichi.sugiyama <caad12.di2@gmail.com>
2024-09-10 21:12:23 +0900

    ルーティング初期設定 #18

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/posiposi/project/backend/internal/api"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	r := api.NewRouter()
+	log.Print("Server is running on port 8080")
+	if err := r.Run(":8081"); err != nil {
+		log.Fatal(err)
+	}
 }
